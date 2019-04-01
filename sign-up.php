5fcce4c81452090e88ac2bdc366e5b3c81e23be7
akshaydesai <adesai16>
2019-04-01 10:52:39 -0400

    changes

---
+++ sign-up.php
@@ -0,0 +1,19 @@
+<?php include "inc/header.inc"; ?>
+<title>Newsletter Sign-up</title>
+<body>
+<?php include "inc/navbar.inc"; ?>
+<div>
+<form action="success.php">
+    First name:<br>
+    <input type="text" name="firstname" size="40"><br><br>
+    Last name:<br>
+    <input type="text" name="lastname" size="40"><br><br>
+    Email:<br>
+    <input type="text" name="lastname" size="40"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
+</div>
+    <?php include "inc/scripts.inc"; ?>
+</body>
+</form>
---
+++ success.php
@@ -0,0 +1,9 @@
+<?php include "inc/header.inc"; ?>
+<title>Newsletter Sign-up</title>
+<body>
+<?php include "inc/navbar.inc"; ?>
+<h1 class="success">Successfully Signed Up!</h1>
+<p class="success">Congratulations. You have signed up for our weekly newsletter.</p>
+<?php include "inc/scripts.inc"; ?>
+</body>
+</form>
